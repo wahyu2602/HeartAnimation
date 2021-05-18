43693dc26abb3d28e8b7b58b0c5854ef32ef5e21
wahyu2602 <wahyupradit@gmail.com>
2021-05-19 02:53:57 +0700

    Upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const heart = document.getElementById("heart");
+const wahyu = document.getElementById("wahyu");
+const kartika = document.getElementById("kartika");
+
+function animasi() {
+  wahyu.classList.toggle("animasi-wahyu");
+  kartika.classList.toggle("animasi-kartika");
+}
